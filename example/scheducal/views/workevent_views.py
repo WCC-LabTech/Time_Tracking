5bf220d88d7c90dfe1ef5933474649caf7003a2c
Brian Rundel <brian@localhost.localdomain>
2014-01-10 12:38:47 -0500

    fixed

--- example/scheducal/views/workevent_views.py
+++ example/scheducal/views/workevent_views.py
@@ -42,10 +42,8 @@ def work_event_list_for_pay_period(request, pay_period):
         raise ObjectDoesNotExist 
     try:
         start = p.start
-        start.second = 00
         end = datetime.datetime(year=p.end.year, month=p.end.month, day=p.end.day,
                                 hour=23, minute=59)
-        end.second = 00
         events = WorkEvent.objects.filter(user=user) \
                                   .filter(start__gte=start) \
                                   .filter(start__lte=end)
