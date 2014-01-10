2cd96459ca08c1adf5a7fba2cbbff0ddb50134cf
Brian Rundel <brian@localhost.localdomain>
2014-01-10 12:37:17 -0500

    fixed

--- example/scheducal/views/workevent_views.py
+++ example/scheducal/views/workevent_views.py
@@ -44,7 +44,8 @@ def work_event_list_for_pay_period(request, pay_period):
         start = p.start
         start.second = 00
         end = datetime.datetime(year=p.end.year, month=p.end.month, day=p.end.day,
-                                hour=23, minute=59,second=00)
+                                hour=23, minute=59)
+        end.second = 00
         events = WorkEvent.objects.filter(user=user) \
                                   .filter(start__gte=start) \
                                   .filter(start__lte=end)
