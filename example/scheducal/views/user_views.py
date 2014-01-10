d8bbf570f659805ef71e256639d06915add4547f
root bosapp <root@localhost.localdomain>
2014-01-10 12:02:12 -0500

    fixes

--- example/scheducal/views/user_views.py
+++ example/scheducal/views/user_views.py
@@ -22,24 +22,24 @@ def user_detail(request, pk):
 def user_delete(user_id):
     user = User.objects.get(all).filter(user_id.POST['pk'])
     status_code = ""
-    try
+    try:
        user.delete()
        status_code = "201"
-    except
+    except:
        status_code = "400" 
     return HttpResponse(status_code)
 
 @require_http_methods(['POST'])
 def create_user(user):    
     status_code =""
-    try
+    try:
        new_user = User()
        new_user.user_name = user.POST['user_name']
        new_user.first_name = user.POST['first_name']
        new_user.last_name = user.POST['last_name']
        new_user.group = user.POST['group'] 
        status_code = "201"
-    except
+    except:
        status_code = "401"
     return HttpResponse(status_code)
 
@@ -49,13 +49,13 @@ def update_user(user):
 	updated_user = User().objects(all).filter(user.POST['pk'])
 	try:
 	   """updated_user.user_name  = user.POST['user_name']
-	     updated_user.first_name = user.POST['first_name']
-	     updated_user.last_name  = user.POST['last_name']
-	     updated_user.group      = user.POST['group']"""
-	     modify_user(user)
+	      updated_user.first_name = user.POST['first_name']
+	      updated_user.last_name  = user.POST['last_name']
+	      updated_user.group      = user.POST['group']"""
+	   modify_user(user)
 	   status_code = "201"
 	except:
-	    status_code = "401" 
+	   status_code = "401" 
 	return HttpResponse(status_code)
 # One function to rule them all
 @require_http_methods(['POST'])
