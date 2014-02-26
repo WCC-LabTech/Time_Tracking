6ce65e279c7ec197b9f70c60e37222ac39810ee3
Brian Rundel <brundel@localhost.localdomain>
2014-02-26 12:28:22 -0500

    killed bug in update user with fire!!!

--- example/scheducal/lib/user_helper.py
+++ example/scheducal/lib/user_helper.py
@@ -1,6 +1,8 @@
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import Group
+from django.utils import simplejson
+
 def user_dict(user):
     groups = [group.pk for group in user.groups.all()]
     return {
